5b7ed44f2becb790a00901d75f5052059c6f56f8
Vadim Velikodniy <vadim@velikodniy.name>
2014-01-27 20:06:52 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-gcc -c dht.c -Wall -Wextra -Wconversion -I./bcm2835 -L./bcm2835 -lbcm2835
-gcc -c base.c -Wall -Wextra -Wconversion -lsqlite3
-gcc -c server.c -Wall -Wno-unused-parameter -Wextra -Wconversion -lmicrohttpd -lsqlite3
-gcc -c dht_daemon.c -Wall -Wextra -Wconversion
-gcc dht.o base.o server.o dht_daemon.o -o dht_daemon -lsqlite3 -lmicrohttpd -I./bcm2835 -L./bcm2835 -lbcm2835
+gcc -c dht.c -Wall -Wextra -Wconversion -I./bcm2835
+gcc -c base.c -Wall -Wextra -Wconversion 
+gcc -c server.c -Wall -Wno-unused-parameter -Wextra -Wconversion
+gcc -c dht_daemon.c -Wall -Wno-unused-parameter -Wextra -Wconversion
+gcc dht_daemon.c dht.o base.o server.o\
+	-o dht_daemon\
+	-lsqlite3 -lmicrohttpd -lbcm2835\
+	-L./bcm2835 
