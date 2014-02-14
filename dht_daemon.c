319b0f10a6fe2a530ea11a43996dae7dfd67097a
Vadim Velikodniy <vadim@velikodniy.name>
2014-02-14 14:19:24 +0200

    Some refactoring

--- dht_daemon.c
+++ dht_daemon.c
@@ -19,28 +19,32 @@
 // Server parameters
 sqlite3 *db_server;
 server_t server;
+int port = -1;
 
 // Sensor reader parameters
 sqlite3* db_sensor;
 unsigned int timeint = 0;
 
-void on_timer_sig(int arg) {
+// DB parameters
+char* dbpath = NULL;
+
+void on_timer_sig (int arg) {
   double temp, hum;
 #ifdef DEBUG
-  fprintf(stderr, "Got SIGALRM\n");
+  fprintf (stderr, "Got SIGALRM\n");
 #endif
-  dht_get(&temp, &hum);
-  base_save(db_sensor, temp, hum);
-  alarm(timeint);
+  dht_get (&temp, &hum);
+  base_save (db_sensor, temp, hum);
+  alarm (timeint);
 }
 
 void on_exit_sig(int arg) {
 #ifdef DEBUG
-  fprintf(stderr, "Exit\n");
+  fprintf (stderr, "Exit\n");
 #endif
-  server_stop(server);
-  base_close(db_server);
-  exit(1);
+  server_stop (server);
+  base_close (db_server);
+  exit (1);
 }
 
 void print_usage(char *name) {
@@ -54,14 +58,54 @@ void print_usage(char *name) {
 
 }
 
-int main(int argc, char **argv) {
-  int port = -1;
+void http_start (void) {
+  int ret;
+#ifdef DEBUG
+  fprintf (stderr, "Starting HTTP-server...");
+#endif
+  ret = base_init (&db_server, dbpath);
+  if (ret != 0) {
+    fprintf (stderr, "Cannot open base '%s'\n", dbpath);
+    exit (1);
+  }
+  
+  server = server_start(port, db_server);
+  if (server == NULL) {
+    fprintf (stderr, "Cannot start server on port %d\n", port);
+    exit (1);
+  }
+#ifdef DEBUG
+  fprintf (stderr, "OK\n");
+#endif
+}
 
+void sensor_start(void) {
+  int ret;
+#ifdef DEBUG
+  fprintf (stderr, "Starting sensor reader...");
+#endif
+  ret = dht_init();
+  if (ret != 0) {
+    fprintf (stderr, "Cannot init sensor\n");
+    exit (1);
+  }
+  ret = base_init(&db_sensor, dbpath);
+  if (ret != 0) {
+    fprintf (stderr, "Cannot open base '%s'\n", dbpath);
+    exit (1);
+  }
+  signal (SIGALRM, &on_timer_sig);
+  alarm (timeint);
+#ifdef DEBUG
+  fprintf (stderr, "OK\n");
+#endif
+}
+
+int main (int argc, char **argv) {
   int ret;
   int i;
 
   char* port_s = NULL;
-  char* dbpath = NULL;
   char* timeint_s = NULL;
 
   int http_only = 0;
@@ -72,18 +116,18 @@ int main(int argc, char **argv) {
   while ((c = getopt (argc, argv, "hp:d:t:s")) != -1)
     switch (c) {
     case 'h':
-      print_usage(argv[0]);
+      print_usage (argv[0]);
       return 0;
     case 'p':
       port_s = optarg;
-      port = atoi(port_s);
+      port = atoi (port_s);
       break;
     case 'd':
       dbpath = optarg;
       break;
     case 't':
       timeint_s = optarg;
-      ret = atoi(timeint_s);
+      ret = atoi (timeint_s);
       timeint = ret < 0 ? 0 : (unsigned int)ret;
       break;
     case 's':
@@ -100,7 +144,7 @@ int main(int argc, char **argv) {
 	fprintf (stderr,
 		 "Unknown option character `\\x%x'.\n",
 		 optopt);
-      return 1;
+      exit (1);
     default:
       abort ();
     }
@@ -117,57 +161,24 @@ int main(int argc, char **argv) {
   if (optind < argc) {
     for (i = optind; i < argc; i++)
       printf ("Non-option argument %s\n", argv[i]);
-    return 0;
+    exit (1);
   }
 
   // Start HTTP-server
-#ifdef DEBUG
-  fprintf (stderr, "Starting HTTP-server...");
-#endif
-  ret = base_init(&db_server, dbpath);
-  if (ret != 0) {
-    fprintf(stderr, "Cannot open base '%s'\n", dbpath);
-    return 1;
-  }
-    
-  server = server_start(port, db_server);
-  if (server == NULL) {
-    fprintf(stderr, "Cannot start server on port %d\n", port);
-    return 1;
-  }
-#ifdef DEBUG
-  fprintf (stderr, "OK\n");
-#endif
-
-  // Register signal handlers
-  signal(SIGTERM, &on_exit_sig);
-  signal(SIGINT,  &on_exit_sig);
+  http_start ();
 
   // Start sensor reader
   if (!http_only) {
-#ifdef DEBUG
-    fprintf (stderr, "Starting sensor reader...");
-#endif
-    ret = dht_init();
-    if (ret != 0) {
-      fprintf(stderr, "Cannot init sensor\n");
-      return 1;
-    }
-    ret = base_init(&db_sensor, dbpath);
-    if (ret != 0) {
-      fprintf(stderr, "Cannot open base '%s'\n", dbpath);
-      return 1;
-    }
-    signal(SIGALRM, &on_timer_sig);
-    alarm(timeint);
-#ifdef DEBUG
-    fprintf (stderr, "OK\n");
-#endif
+    sensor_start ();
   }
 
+  // Register exit signal handlers
+  signal (SIGTERM, &on_exit_sig);
+  signal (SIGINT,  &on_exit_sig);
+
   // Main loop
-  while(1)
-    pause();
+  while (1)
+    pause ();
 
   return 0;
 }
